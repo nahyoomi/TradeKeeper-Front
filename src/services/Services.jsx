25ee0df7a9d32dd6d06fe6ec072f3052ccb182d6
U-IKEASPC\nahomi.conde <nahomi.conde@GCA-BBX-WKS110.ikeaspc.ikeasi.com>
2023-07-04 13:21:52 +0100

    :monocle_face: Data exploration with axios

---
+++ src/services/Services.jsx
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+let URL_MAIN = "http://localhost:8080";
+
+export const createItem = async(data) =>{
+    let URL =`${URL_MAIN}/item`;
+    let response = await axios.post(URL, data);
+    return response;
+};
+
+export const getItems = async() => {
+    let URL =`${URL_MAIN}/item/items`;
+    let response = await axios.get(URL);
+    return response;
+};
+
+export const updateItem = async(data) => {
+    let URL = `${URL_MAIN}/item/update`;
+    let response = await axios.put(URL, data);
+    return response;
+}
+
+export const deactivateItem = async(data) => {
+    let URL = `${URL_MAIN}/item/remove`;
+    let response = await axios.delete(URL, data);
+    return response;
+}
